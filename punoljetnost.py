ab6f876498f40a53055faa19b7abdc89a5d142ea
Tarik <tare.po08@gmail.com>
2024-07-19 19:08:28 +0200

    Create punoljetnost.py

---
+++ punoljetnost.py
@@ -0,0 +1,9 @@
+godiste_unos = int(input("Unesi svoje godiste: "))
+
+def godiste (godiste_unos):
+    if godiste_unos <=2006:
+        print("Punoljetni ste")
+    else:
+
+        print("Niste punoljetni")
+godiste(godiste_unos)
